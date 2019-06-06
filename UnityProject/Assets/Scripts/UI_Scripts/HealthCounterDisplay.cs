37e36fe81105ded24585e31d26a35bf78e092d04
Grace Willhoite <>
2019-06-06 14:17:34 -0700

    UI Canvases for a MainMenu, Settings, GameOver, GameOverSettings and HealthTracker. Currently SpaceBar decrements the health while in game. Volume changing in settings needs an audio source (some kind of music or effect)

---
+++ UnityProject/Assets/Scripts/UI_Scripts/HealthCounterDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthCounterDisplay : MonoBehaviour
+{
+    // Will need to be connected to the player controller
+    private int health;
+
+    public Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = health.ToString();
+        
+        // Test that the UI changes
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health--;
+        }
+    }
+}
