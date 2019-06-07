30eb2a5f53f78283fa597d1ac4361c3fb6633424
coreymason <corey.mason98@googlemail.com>
2019-06-07 02:04:04 -0700

    Connect UI Part 1

---
+++ UnityProject/Assets/Scripts/UI/ChangeVolume.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Zenject;
+
+public class ChangeVolume : MonoBehaviour
+{
+    private AudioManager _audioManager;
+    [FormerlySerializedAs("Volume")] public Slider volume;
+
+    [Inject]
+    private void Init(AudioManager audioManager)
+    {
+        _audioManager = audioManager;
+    }
+    
+    void Update()
+    {
+        _audioManager.setVolume(volume.value);
+    }
+}
---
+++ UnityProject/Assets/Scripts/UI/HealthCounterDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class HealthCounterDisplay : MonoBehaviour
+{
+    private Player _player;
+    
+    private int _health;
+    public Text healthText;
+ 
+    [Inject]
+    private void Init(Player player)
+    {
+        _player = player;
+    }
+
+    void Update()
+    {
+        healthText.text = _player.Health.ToString();
+    }
+}
