8308f5070a6450b18b297b32d8d607c252f4d638
coreymason <corey.mason98@googlemail.com>
2019-06-11 11:49:25 -0700

    Add Enemy Animators & Add Sprite Direction Flip

--- UnityProject/Assets/Scripts/Actors/EnemyController.cs
+++ UnityProject/Assets/Scripts/Actors/EnemyController.cs
@@ -5,11 +5,13 @@ using Zenject;
 [RequireComponent(typeof(PeacefulAI))]
 [RequireComponent(typeof(HostileAI))]
 [RequireComponent(typeof(AIPath))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class EnemyController : MonoBehaviour
 {
     private Enemy _enemy;
     private PeacefulAI _peacefulAI;
     private HostileAI _hostileAI;
+    private SpriteRenderer _spriteRenderer;
     
     private AIPath _aiPath;
     
@@ -26,6 +28,7 @@ public class EnemyController : MonoBehaviour
         _peacefulAI = GetComponent<PeacefulAI>();
         _hostileAI = GetComponent<HostileAI>();
         _aiPath = GetComponent<AIPath>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         
         _peacefulAI.enabled = true;
         _hostileAI.enabled = false;
@@ -38,10 +41,12 @@ public class EnemyController : MonoBehaviour
         if (_aiPath.velocity.x < 0)
         {
             IsMovingRight = false;
+            _spriteRenderer.flipX = true;
         }
         else if (_aiPath.velocity.x > 0)
         {
             IsMovingRight = true;
+            _spriteRenderer.flipX = false;
         }
     }
 
