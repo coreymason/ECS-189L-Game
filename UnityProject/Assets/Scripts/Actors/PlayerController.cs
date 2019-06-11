1937e89a1fc5c1d7c7e18b4d192d0e538b0d0327
BelleZhou <xuczhou@ucdavis.edu>
2019-06-10 17:57:28 -0700

    fixed running and shooting

--- UnityProject/Assets/Scripts/Actors/PlayerController.cs
+++ UnityProject/Assets/Scripts/Actors/PlayerController.cs
@@ -49,16 +49,13 @@ public class PlayerController : MonoBehaviour
     // TODO: Fix state change bugs, explore the original velocity + smoothing method vs translate, fix MKB/controller bug as in the original
     private void Move()
     {
-        float actualSpeed;
+        float actualSpeed=0;
         Vector2 moveVelocity = new Vector2(_inputManager.Horizontal, _inputManager.Vertical);
         if (_moveState == "walking")
         {
-            Debug.Log("the speed front is at beginning" + animator.GetFloat("speed_front"));
             actualSpeed = moveSpeed;
             animator.SetFloat("speed_front",actualSpeed*_inputManager.Vertical);
             animator.SetFloat("speed_right",actualSpeed*_inputManager.Horizontal);
-            Debug.Log("the speed front is" + animator.GetFloat("speed_front"));
-            //Debug.Log(actualSpeed*_inputManager.Horizontal);
             _movementDirection = new Vector2(_inputManager.Horizontal, _inputManager.Vertical);
             _velocity = _movementDirection * actualSpeed;
             rb.velocity = _velocity;
@@ -83,7 +80,7 @@ public class PlayerController : MonoBehaviour
                 _moveState = "walking";
                 _timer = 0.0f;
             }
-        }
+        }  
     }
 
     // TODO: Use projectile factory
@@ -92,14 +89,66 @@ public class PlayerController : MonoBehaviour
         Vector3 mousePos = _inputManager.FirePosition;
         if (_inputManager.Fire)
         {
-            animator.SetTrigger("is_fire");
+            //animator.SetTrigger("is_fire");
             float t = mousePos.x - transform.position.x;
             float u = mousePos.y - transform.position.y;
             var theta = Mathf.Atan(u / t);
+           
             var degtheta = theta * Mathf.Rad2Deg;
             if (t < 0)
             {
                 degtheta -= 180;
+            }
+            if (t > 0)
+            {
+                if (u > 0)
+                {
+                    if (u / t > 1)
+                    {
+                        animator.SetTrigger("is_back_fire");
+                    }
+                    else
+                    {
+                        animator.SetTrigger("is_right_fire");
+                    }
+                }
+                else
+                {
+                    if (u / t > -1)
+                    {
+                        animator.SetTrigger("is_right_fire");
+                    }
+                    else
+                    {
+                        animator.SetTrigger("is_front_fire");
+                    }
+                }
+                
+            }
+            else
+            {
+                if (u > 0)
+                {
+                    if (u / t > -1)
+                    {
+                        animator.SetTrigger("is_left_fire");
+                    }
+                    else
+                    {
+                        animator.SetTrigger("is_back_fire");
+                    }
+                }
+                else
+                {
+                    if (u / t > 1)
+                    {
+                        animator.SetTrigger("is_front_fire");
+                    }
+                    else
+                    {
+                        animator.SetTrigger("is_left_fire");
+                    }
+                }
             }
 
             var projectileRotation = Quaternion.Euler(0, 0, degtheta);
